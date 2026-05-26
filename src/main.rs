93fede80bacb538b59643baddab227ad325db720
leet-man <leet-man@outlook.com>
2026-05-26 14:04:15 +1000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,31 @@ use std::io;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number! (1–100)");
 
-    let mut guesses: i32 = 0;
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+    let mut guesses: u32 = 0;
 
     loop {
         println!("Please input guess number {}", guesses + 1);
-        let mut guess = String::new();
 
+        let mut input = String::new();
         io::stdin()
-            .read_line(&mut guess)
+            .read_line(&mut input)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: u32 = match input.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Please enter a valid number.");
+                continue;
+            }
         };
+
+        if !(1..=100).contains(&guess) {
+            println!("Please guess a number between 1 and 100.");
+            continue;
+        }
 
         println!("You guessed: {guess}");
         guesses += 1;
@@ -29,10 +37,9 @@ fn main() {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You win! You took {} guesses.", guesses);
                 break;
             }
         }
     }
-    println!("You took {} guesses!", guesses);
 }
